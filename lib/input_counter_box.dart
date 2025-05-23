fc67c983e02f81c7b6841044f94c2e73783eda07
JuergenKuck <drj.kuck@googlemail.com>
2025-05-23 11:47:58 +0200

    Fertig

--- lib/input_counter_box.dart
+++ lib/input_counter_box.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class InputCounterBox extends StatefulWidget {
-  const InputCounterBox({super.key});
+  final Color backGroundcolor;
+  final Function(int) onLengthChange;
+
+  const InputCounterBox({
+    super.key,
+    required this.backGroundcolor,
+    required this.onLengthChange,
+  });
 
   @override
   State<InputCounterBox> createState() => _InputCounterBoxState();
@@ -13,6 +20,7 @@ class _InputCounterBoxState extends State<InputCounterBox> {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: widget.backGroundcolor,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -22,6 +30,7 @@ class _InputCounterBoxState extends State<InputCounterBox> {
               onChanged: (text) {
                 setState(() {
                   _length = text.length;
+                  widget.onLengthChange(_length);
                 });
               },
               decoration: InputDecoration(
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,8 +1,17 @@
 import 'package:callback_functions/input_counter_box.dart';
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int len1 = 0;
+  int len2 = 0;
+  int len3 = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +21,29 @@ class MainScreen extends StatelessWidget {
         child: Column(
           spacing: 16,
           children: [
-            InputCounterBox(),
-            InputCounterBox(),
-            InputCounterBox(),
+            InputCounterBox(
+              backGroundcolor: Colors.lime,
+              onLengthChange: (textLenght) {
+                len1 = textLenght;
+                setState(() {});
+              },
+            ),
+            InputCounterBox(
+              backGroundcolor: Colors.cyan,
+              onLengthChange: (textLenght) {
+                len2 = textLenght;
+                setState(() {});
+              },
+            ),
+            InputCounterBox(
+              backGroundcolor: Colors.purpleAccent,
+              onLengthChange: (textLenght) {
+                len3 = textLenght;
+                setState(() {});
+              },
+            ),
+            SizedBox(height: 16),
+            Text("Gesmattextlänge = $len1 + $len2 + $len3 = ${len1 + len2 + len3}"),
           ],
         ),
       ),
